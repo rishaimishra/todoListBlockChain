e1308364af464cf7a26dfa6a571cb5f3c279d00f
srvtechnology <srvtechnology0@gmail.com>
2022-05-06 21:43:39 +0530

    todolist and migration add

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,6 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList{
     uint public taskCount = 0;
-    
+
+    struct Task {
+        uint id;
+        string content;
+        bool isDone;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public{
+        createTask("check my first project");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 
 }
